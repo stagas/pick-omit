c9ee1944c31cfc7d32cfc710ae4f98d55df84955
stagas <gstagas@gmail.com>
2022-07-30 00:45:50 +0300

    chore: repo

--- test/pick-omit.spec.ts
+++ test/pick-omit.spec.ts
@@ -1,13 +1,15 @@
-import { inspectWithPreamble } from '@n1kk/intspector'
-import { omit, pick } from '../src'
+import { inspectWithPreamble, setOptions } from '@n1kk/intspector'
+import { nonNull, omit, pick } from '../src'
+
+setOptions(require('../tsconfig.json'), true)
 
 const typeTest = (fn: string) => {
   try {
-    inspectWithPreamble(`
-      import { omit, pick } from '../src'
+    const { result } = inspectWithPreamble(`
+      import { omit, pick, nonNull } from '../src'
       const result = ${fn}
     `)({ result: 'typeof result' })
-    return true
+    return result
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return error[0].messageText
@@ -23,7 +25,9 @@ describe('all', () => {
     })
 
     it('creates correct type', () => {
-      expect(typeTest(`pick({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c'])`)).toEqual(true)
+      expect(typeTest(`pick({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c'])`)).toEqual(
+        `{ a: string; b: string; c: string; }`
+      )
       // expect(pick({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c', 'd'])).toEqual({ a: 'a', b: 'b', c: 'c' })
       expect(typeTest(`pick({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c', 'd'])`)).toContain(
         'Type \'"d"\' is not assignable to type \'"a" | "b" | "c"\'.'
@@ -39,10 +43,25 @@ describe('all', () => {
     })
 
     it('creates correct type', () => {
-      expect(typeTest(`omit({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c'])`)).toEqual(true)
+      expect(typeTest(`omit({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c'])`)).toEqual(
+        `{}`
+      )
       // expect(omit({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c', 'd'])).toEqual({})
       expect(typeTest(`omit({ a: 'a', b: 'b', c: 'c' }, ['a', 'b', 'c', 'd'])`)).toContain(
         'Type \'"d"\' is not assignable to type \'"a" | "b" | "c"\'.'
+      )
+    })
+  })
+
+  describe('nonNull', () => {
+    it('picks non null values', () => {
+      expect(nonNull({ a: 'a', b: null, c: undefined })).toEqual({ a: 'a' })
+    })
+
+    it('creates correct type', () => {
+      expect(typeTest(`nonNull({ a: 'a', b: null, c: undefined })`)).toEqual(
+        // TODO: some weird reason these become any instead of being removed
+        `{ a: string; b: any; c: any; }`
       )
     })
   })
