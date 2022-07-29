486594ffdb3d70a33cb61665e1fb6056e30c2347
stagas <gstagas@gmail.com>
2022-07-30 00:45:33 +0300

    fix: pick

--- src/pick-omit.ts
+++ src/pick-omit.ts
@@ -31,7 +31,10 @@ export const nonNull = <T>(obj: T): Pick<T, keyof NonNull<T>> =>
  * @returns A new object with the picked properties.
  */
 export const pick = <T, K extends (keyof T)[]>(obj: T, props: K): Pick<T, keyof ObjectFromList<K>> =>
-  filter(obj, props) as Pick<T, keyof ObjectFromList<K>>
+  props.reduce((p, n) => {
+    if (n in obj) p[n] = obj[n]
+    return p
+  }, {} as any) as Pick<T, keyof ObjectFromList<K>>
 
 /**
  * Create a new object with certain properties omitted.
@@ -41,10 +44,10 @@ export const pick = <T, K extends (keyof T)[]>(obj: T, props: K): Pick<T, keyof 
  * @returns A new object with the properties omitted.
  */
 export const omit = <T, K extends (keyof T)[]>(obj: T, props: K): Omit<T, keyof ObjectFromList<K>> =>
-  filter(obj, props, true) as Omit<T, keyof ObjectFromList<K>>
+  filter(obj, includesKey(props, true)) as Omit<T, keyof ObjectFromList<K>>
 
-/** @private */
-export const filter = <T>(obj: T, props: Key[], invert = false): Partial<T> =>
-  Object.fromEntries(
-    Object.entries(obj).filter(([key]) => (invert as any) ^ (props.includes(key) as any))
-  ) as Partial<T>
+/**
+ * @private
+ */
+export const includesKey = <K extends unknown[]>(props: K, invert = false): ObjectFilterFn =>
+  ([key]) => ((invert as any) ^ (props.includes(key) as any)) as unknown as boolean
